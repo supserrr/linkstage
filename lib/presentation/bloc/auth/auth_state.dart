c4df11d6e1f9929eeecf23d504f2258201ce323f
Christian <p.uhiriwe@alustudent.com>
2026-03-08 04:31:54 +0200

    feat(presentation): add auth bloc with events and states
    Implement AuthBloc handling email link sign-in, Google sign-in,
    and sign-out flows using clean architecture use cases.

---
+++ lib/presentation/bloc/auth/auth_state.dart
@@ -0,0 +1,49 @@
+import 'package:equatable/equatable.dart';
+
+import '../../../domain/entities/user_entity.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+class AuthLoading extends AuthState {
+  const AuthLoading();
+}
+
+class AuthAuthenticated extends AuthState {
+  const AuthAuthenticated(this.user);
+
+  final UserEntity user;
+
+  @override
+  List<Object?> get props => [user];
+}
+
+class AuthUnauthenticated extends AuthState {
+  const AuthUnauthenticated();
+}
+
+class AuthLinkSent extends AuthState {
+  const AuthLinkSent(this.email);
+
+  final String email;
+
+  @override
+  List<Object?> get props => [email];
+}
+
+class AuthError extends AuthState {
+  const AuthError(this.message);
+
+  final String message;
+
+  @override
+  List<Object?> get props => [message];
+}
